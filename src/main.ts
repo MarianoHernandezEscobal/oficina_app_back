a17c16f1c1b3dbf6afa5e00088585949f137b163
MarianoHernandezEscobal <mh202428@fi365.ort.edu.uy>
2025-07-04 10:36:16 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 10000;
-
+  app.enableCors({
+    origin: ['http://localhost:8100'],
+    credentials: true,
+  });
+  
   await app.listen(port);
 }
 bootstrap();
